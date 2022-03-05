d05072d52c36956685476c6194bd56b50e772d04
Syed Salman <37833818+alphamod@users.noreply.github.com>
2022-03-05 22:46:14 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+// const loginFunc = (event) => {
+//   const email = document.getElementById("email").value;
+//   const password = document.getElementById("password").value;
+
+//   fetch("http://localhost:3000/login", 
+//   {
+//       method : "POST",
+//       body: {email, password},
+//       headers: {
+//         'Accept': 'application/json',
+//         'Content-Type': 'application/x-www-form-urlencoded'
+//       }
+//     })
+//   .then((response) => response.text())
+//   .then((result) => console.log(result));
+// };
