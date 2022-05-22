ef2bf30d42f8ae404963b9f6384a0b0156771be9
Syed Salman <37833818+alphamod@users.noreply.github.com>
2022-05-22 21:54:59 +0530

    22-05-2022
    js - comparators & logical operators.

---
+++ old.js
@@ -0,0 +1,50 @@
+
+// var fruits = ["Apples", "Oranges", "Strawberry", 124, true, [23, 56, "mhcjdgf"]];
+// console.log("before",fruits); // ["Apples", "Oranges", "Strawberry", 124, true, [23, 56, "mhcjdgf"]];
+// // fruits.push("Monkey");
+// var myVar = fruits.pop(); // fruits = ["Apples", "Oranges", "Strawberry", 124, true]; myVar = [23, 56, "mhcjdgf"]
+// console.log("myvar ---> first pop",myVar); //  true
+// myVar = fruits.pop(); // fruits = ["Apples", "Oranges", "Strawberry", 124] ; 
+// console.log("myvar ---> second pop",myVar); //  true
+// console.log("after",fruits); // ["Apples", "Oranges", "Strawberry", 124] ;
+// console.log("last_item",myVar); //  true
+
+
+var a = 5;
+var b = 10;
+var d = a;
+d = 15;
+
+// console.log(c);
+// console.log(a);
+
+var snacks = ["banana", "chips", "apples", 54, 8976, "apples", 645, "apples"];
+var new_snacks = snacks; //mutable
+var other = snacks.slice(); //immutable
+
+var sub_array = snacks.slice(1,6);
+console.log(sub_array)
+// snacks.splice(2, 3)
+
+// console.log("before unshift",snacks);
+// var firstItem = snacks.unshift("oranges")
+// console.log(snacks);
+// console.log(firstItem)
+
+
+
+// var indexOfApples = snacks.indexOf("apples");
+
+// console.log("index", indexOfApples)
+// console.log()
+// arrayName.splice(index, no. of items to be removed, "new items to be added seperated by commas")
+
+// var sub_snacks = snacks.slice(2,5);
+
+// console.log(sub_snacks);
+
+
+// // console.log("first other", other);
+// other.pop();
+
+// // console.log("other", other);
--- script.js
+++ script.js
@@ -1,21 +1,48 @@
-// const loginFunc = (event) => {
-//   const email = document.getElementById("email").value;
-//   const password = document.getElementById("password").value;
 
-//   fetch("http://localhost:3000/login", 
-//   {
-//       method : "POST",
-//       body: {email, password},
-//       headers: {
-//         'Accept': 'application/json',
-//         'Content-Type': 'application/x-www-form-urlencoded'
-//       }
-//     })
-//   .then((response) => response.text())
-//   .then((result) => console.log(result));
-// };
+// operators  + - * / % ++ -- ** =
 
+// ==, !=, < , > , <=, >=, ===, !==
 
-var a = "24is76is54=yjistdu=hisgfjisht"
-var one = [23, "apple", 1, 1000, 653]
-console.log(a.split("is"));
+// AND &&,  OR ||, NOT !
+
+// console.log("abdul" != "abdul");
+
+var firstName = "Abdul";
+var timeOfDay = "noon";
+var a = 100;
+var b = "100a";
+var c = a*b;
+var n = null;
+// var c = false;
+var d = false;
+var firstName = prompt("Enter first name:");
+var lastName = prompt("Enter last name:");
+var s = ""  // falsy value
+var r = "nhgfc"; //true
+var g = 0; // falsy value    // 0, "", null, undefined, NaN, false   - falsy values
+
+if (!firstName || !lastName){
+    if(!firstName && !lastName){
+        firstName = prompt("please enter first name:");
+        lastName = prompt("please enter last name:");
+    } else if(!lastName){
+        lastName = prompt("please enter last name:")
+    }else { 
+        firstName = prompt("please enter first name:")
+    }
+} else {
+    alert("full name is "+ firstName+ " "+ lastName);
+};
+
+// if (timeOfDay == "morning"){
+//     console.log("Good Morning"+ ", "+ firstName+"!");
+// } else if(timeOfDay == "evening"){
+//     console.log("Good Evening, "+ firstName+"!");
+// }else if(timeOfDay == "noon"){
+//     console.log("Good noon, "+ firstName+"!");
+// } else if(timeOfDay == "night"){
+//     console.log("Good night, "+ firstName+"!");
+// }
+//  else{
+//     console.log("Hello, "+ firstName+"!")
+// }
