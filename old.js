620ff2024a827903ba54c56fcf98b5546d6b1aa1
Syed Salman <37833818+alphamod@users.noreply.github.com>
2022-07-03 17:23:26 +0530

    node client

--- old.js
+++ old.js
@@ -199,3 +199,342 @@ function login(username, password, comparingFunction) {
     img.src = responseFromBackend.profilePic;
     document.querySelector("#loginBtn").style.display="none"
   }
+
+  // DOM
+
+  // DOM = Document Object Model
+
+var employees = [
+  {
+  "userId":"rirani",
+  "jobTitleName":"Developer",
+  "firstName":"Romin",
+  "lastName":"Irani",
+  "preferredFullName":"Romin Irani",
+  "employeeCode":"E1",
+  "region":"CA",
+  "phoneNumber":"408-1234567",
+  "emailAddress":"romin.k.irani@gmail.com"
+  },
+  {
+  "userId":"nirani",
+  "jobTitleName":"Developer",
+  "firstName":"Neil",
+  "lastName":"Irani",
+  "preferredFullName":"Neil Irani",
+  "employeeCode":"E2",
+  "region":"CA",
+  "phoneNumber":"408-1111111",
+  "emailAddress":"neilrirani@gmail.com"
+  },
+  {
+  "userId":"thanks",
+  "jobTitleName":"Program Directory",
+  "firstName":"Tom",
+  "lastName":"Hanks",
+  "preferredFullName":"Tom Hanks",
+  "employeeCode":"E3",
+  "region":"CA",
+  "phoneNumber":"408-2222222",
+  "emailAddress":"tomhanks@gmail.com"
+  }
+]
+
+var tbody = document.getElementById("tbody");
+
+for (i=0; i<employees.length; i++){
+  var tr = document.createElement('tr');
+  var employeeCode = document.createElement('td')
+  var jobTitle = document.createElement('td')
+  var fullName = document.createElement('td')
+  var email = document.createElement('td')
+  var region = document.createElement('td')
+  employeeCode.innerText = employees[i].employeeCode;
+  jobTitle.innerText = employees[i].jobTitleName;
+  fullName.innerText = employees[i].preferredFullName;
+  email.innerText = employees[i].emailAddress;
+  region.innerText = employees[i].region;
+  tr.appendChild(employeeCode);
+  tr.appendChild(jobTitle);
+  tr.appendChild(fullName);
+  tr.appendChild(email);
+  tr.appendChild(region);
+  tbody.appendChild(tr);
+}
+
+var firstTh = document.querySelector("#firstTh")
+firstTh.style.border = "none"
+
+function sample(){
+  alert("i am clicked!!")
+}
+
+
+function handleSubmit(){
+  var username = document.getElementById("username").value
+  
+  document.getElementById("formUsername").innerText = username;
+}
+
+
+// DOM events
+
+function handleChange(event){
+  console.log(event.target.value)
+
+}
+
+function handleEvent(event){
+  console.log(event)
+
+}
+
+var handleEnterKey = function(event){
+  console.log(event)
+  if(event.key === "Enter"){
+    alert("Enter is pressed!")
+  }
+}
+
+document.getElementById('input').addEventListener("keydown", handleEnterKey)
+
+function stopEvent(){
+  document.removeEventListener("keydown",handleEnterKey);
+}
+
+// event bubbling
+
+var handleBody = function(event){
+  alert("event triggered - body");
+  
+}
+var handleForm = function(event){
+  alert("event triggered - form");
+  
+}
+
+var handleButton = function(event){
+  alert("event triggered - button");
+  
+}
+var handleDiv = function(event){
+  alert("event triggered - DIv");
+  
+}
+
+document.getElementById('body').addEventListener("click", handleBody)
+document.getElementById('form').addEventListener("click", handleForm)
+document.getElementById('button').addEventListener("click", handleButton)
+document.getElementById('formParent').addEventListener("click", handleDiv)
+
+
+function stopEvent(){
+  document.removeEventListener("keydown",handleEnterKey);
+}
+
+// API
+// ================
+// GET  -  it will bring data from the backend. //cache data
+// POST - We will send data to backend.  // will not be cached
+// PUT - Modify already existing data.
+// DELETE - Deleting the data.
+
+// vanilla js
+
+var xhttp = new XMLHttpRequest();    //0
+var date = new Date();
+xhttp.onreadystatechange = function() {
+        console.log(this.readyState);
+    if (this.readyState == 4 && this.status == 200) {
+       var response = xhttp.responseText;
+    //    console.log(response)
+    }
+};
+xhttp.open("GET", "https://api.github.com/users/453", true); 
+xhttp.send();
+
+
+// promise
+
+var myPromise = new Promise(function(myResolve, myReject) {
+  // "Producing Code" (May take some time)
+  
+    myResolve("resolved"); // when successful
+    myReject({
+      errType: "DB error",
+      message: "Please check your query"
+    });  // when error
+  });
+  
+  // "Consuming Code" (Must wait for a fulfilled Promise)
+  myPromise
+  .then(function (data){
+      console.log("data in .then is", data);
+      return { message: data};
+  })
+  .then(function(result){
+      console.log(result);
+      return JSON.stringify(result);
+  })
+  .then(function(param){
+      console.log("3rd then",param);
+  })
+  .catch(function(err){
+      console.log("from .catch",err.message);
+  });
+
+// leetcode palindrome integer
+
+try{
+  var isPalindrome = function(x) { 
+      var remainder = 0;
+      var newNumber = x;
+      while(newNumber>0){ // (0>0) ----> false
+          var tempRem = newNumber%10;  // tempRem - 1
+          newNumber = newNumber - tempRem; // 1 - 1 = 0
+          newNumber/=10; // newnumber = 0/10  , 0
+          remainder = (remainder*10)+tempRem; // (32*10)+1 , remainder = 321
+          console.log({remainder, newNumber});
+      }
+      if (remainder === x){ 
+          return true;
+      }else{
+          return false;
+      }
+    };
+
+    console.log(isPalindrome(123));
+
+} catch(err){
+  console.log("error from catch block, an error has been occured")
+}
+
+// var numStr = String(x);
+// var strArr = x.split('') // ['1', '1', '2', '1', '1']
+// strArr = strArr.reverse().join(''); // reverse(), ['1', '1', '2', '1', '1'], at join(''), '11211'
+//  if(x===parseInt(strArr)){  // at parseInt('11211'), ----> 11211
+//      return true;
+//  }else{
+//      return false;
+//  }
+
+// roman to integer
+
+//Convert RomanNumerals to Arabic
+try {
+  //var s = "MCMXCIV";
+
+  function sum (a,b){
+
+    return a+b;
+  };
+
+
+
+function sumOfItems(arr){ //n
+    var sum = 0
+    for(i=0; i<arr.length; i++){
+        sum = sum+arr[i];
+        for(j=1; j<arr.length; j++){
+
+        }
+    }
+    return sum;
+}
+
+  var coun;
+  var sum = 0; // undefined
+  function Rom2Arab(s) {
+    const I = 1;
+    const V = 5;
+    const X = 10;
+    const L = 50;
+    const C = 100;
+    const D = 500;
+    const M = 1000;
+    coun = s.length;
+    var reverse = s.split("").reverse().join("");
+
+    for (let i = 0; i < coun; i++) {
+      switch (reverse[i]) {
+        case "M":
+          if (reverse[i + 1] === "C") {
+            sum = sum + (M - C);
+            i++;
+          } else if (reverse[i + 1] === "D") {
+            sum = sum + (M - D);
+            i++;
+          } else {
+            sum = sum + M;
+          }
+          break;
+        case "D":
+          if (reverse[i + 1] === "C") {
+            sum = sum + (D - C);
+            i++;
+          } else {
+            sum = sum + D;
+          }
+
+          break;
+        case "C":
+          if (reverse[i + 1] === "X") {
+            sum = sum + (C - X);
+            i++;
+          } else {
+            sum = sum + C;
+          }
+
+          break;
+        case "L":
+          if (reverse[i + 1] === "X") {
+            sum = sum + (L - X);
+            i++;
+          } else {
+            sum = sum + L;
+          }
+
+          break;
+        case "X":
+          if (reverse[i + 1] === "I") {
+            sum = sum + (X - I);
+            i++;
+          } else {
+            sum = sum + X;
+          }
+
+          break;
+        case "V":
+          if (reverse[i + 1] === "I") {
+            sum = sum + (V - I);
+            i++;
+          } else {
+            sum = sum + V;
+          }
+
+          break;
+        default:
+          sum = sum + I;
+          break;
+      }
+    }
+    return sum;
+  }
+} catch {}
+
+console.log(Rom2Arab("MCMXCIV")); //1900
+
+// M
+
+// console.log("Char  " + i);
+//             if (s[i] === "I" && s[i + 1] === "V") {
+//                 sum += 4; //
+//                 i++;
+//                 console.log("sum inside if::: " + sum);
+//             }
+//             else if (s[i] === "I" && s[i + 1] === "X") {
+//                 sum += 9; //
+//                 i++;
+//             }
+
+//             console.log("Char  + i is : " + sum);
--- script.js
+++ script.js
@@ -1,71 +1,12 @@
-// DOM = Document Object Model
-
-var employees = [
-  {
-  "userId":"rirani",
-  "jobTitleName":"Developer",
-  "firstName":"Romin",
-  "lastName":"Irani",
-  "preferredFullName":"Romin Irani",
-  "employeeCode":"E1",
-  "region":"CA",
-  "phoneNumber":"408-1234567",
-  "emailAddress":"romin.k.irani@gmail.com"
-  },
-  {
-  "userId":"nirani",
-  "jobTitleName":"Developer",
-  "firstName":"Neil",
-  "lastName":"Irani",
-  "preferredFullName":"Neil Irani",
-  "employeeCode":"E2",
-  "region":"CA",
-  "phoneNumber":"408-1111111",
-  "emailAddress":"neilrirani@gmail.com"
-  },
-  {
-  "userId":"thanks",
-  "jobTitleName":"Program Directory",
-  "firstName":"Tom",
-  "lastName":"Hanks",
-  "preferredFullName":"Tom Hanks",
-  "employeeCode":"E3",
-  "region":"CA",
-  "phoneNumber":"408-2222222",
-  "emailAddress":"tomhanks@gmail.com"
-  }
-]
-
-var tbody = document.getElementById("tbody");
-
-for (i=0; i<employees.length; i++){
-  var tr = document.createElement('tr');
-  var employeeCode = document.createElement('td')
-  var jobTitle = document.createElement('td')
-  var fullName = document.createElement('td')
-  var email = document.createElement('td')
-  var region = document.createElement('td')
-  employeeCode.innerText = employees[i].employeeCode;
-  jobTitle.innerText = employees[i].jobTitleName;
-  fullName.innerText = employees[i].preferredFullName;
-  email.innerText = employees[i].emailAddress;
-  region.innerText = employees[i].region;
-  tr.appendChild(employeeCode);
-  tr.appendChild(jobTitle);
-  tr.appendChild(fullName);
-  tr.appendChild(email);
-  tr.appendChild(region);
-  tbody.appendChild(tr);
-}
-
-var firstTh = document.querySelector("#firstTh")
-firstTh.style.border = "none"
-
-
-
-function handleSubmit(event){
-  event.preventDefault();
-  var username = document.getElementById("username").value
-  
-  document.getElementById("formUsername").innerText = username;
-}
+const getData = () => {
+  const xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      // Typical action to be performed when the document is ready:
+      const response = JSON.parse(xhttp.responseText);
+      document.getElementById("demo").innerHTML = response.data.profilePic;
+    }
+  };
+  xhttp.open("GET", "http://127.0.0.1:3000", true);
+  xhttp.send();
+};
