6f3838d2fa22118bae2ce026ea032ec3c774631f
Syed Salman <37833818+alphamod@users.noreply.github.com>
2022-07-23 00:16:04 +0530

    23/7/22

--- script.js
+++ script.js
@@ -1,12 +1,30 @@
-const getData = () => {
-  const xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function () {
-    if (this.readyState == 4 && this.status == 200) {
-      // Typical action to be performed when the document is ready:
-      const response = JSON.parse(xhttp.responseText);
-      document.getElementById("demo").innerHTML = response.data.profilePic;
-    }
-  };
-  xhttp.open("GET", "http://127.0.0.1:3000", true);
-  xhttp.send();
+const login = (event) => {
+  event.preventDefault();
+  
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+
+  const data = {email, password};
+  console.log({data});
+
+  fetch('http://localhost:8080/login', {
+    method: 'POST',
+    body: JSON.stringify(data),
+    headers: {"Content-type": "application/json; charset=UTF-8"}
+  }) 
+    .then(response => response.json())  // convert to json
+    .then(response =>{ 
+      if(response.message !== "login success"){
+        document.getElementById('err-message').innerText = response.message;
+      }
+    })    //print data to console
+    .catch(err => console.log('Request Failed', err)); // Catch errors if the request fails
+
 };
+
+
+// .then((response) => {
+//   return response.json()
+// })
+
+// {email, password}   -------> {email: email, password: password}
